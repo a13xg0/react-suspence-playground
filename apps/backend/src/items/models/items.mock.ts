543039d6511a68c4cb70356f41580dd993406553
Alex <a.v.gorbach@gmail.com>
2023-12-11 20:08:16 +0400

    add generation of mocked data on the start

---
+++ apps/backend/src/items/models/items.mock.ts
@@ -0,0 +1,17 @@
+import { faker } from '@faker-js/faker'
+import { Item } from './items.model'
+
+const mockedItems: Item[] = []
+
+const ITEMS_TO_MOCK = 20
+
+for (let i = 0; i < ITEMS_TO_MOCK; i++) {
+  mockedItems.push({
+    id: i,
+    title: faker.lorem.sentence(4),
+    description: faker.lorem.sentence(),
+    completed: faker.datatype.boolean(),
+  })
+}
+
+export default mockedItems
