c56bd94717e39c6e7eb2a3a5a908efcea3064db0
Alex <a.v.gorbach@gmail.com>
2023-12-12 16:00:30 +0400

    Update css module names generation to include module name

--- apps/react-frontend/vite.config.ts
+++ apps/react-frontend/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import crypto from 'crypto'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      generateScopedName: (name, filename, css) => {
+        const componentName = filename
+          .replace(/\.\w+$/, '')
+          .split('/')
+          .pop()
+          .split('.')
+          .shift()
+
+        // Generate hash
+        const hash = crypto
+          .createHash('md5')
+          .update(css)
+          .digest('base64')
+          .substring(0, 5)
+
+        return `${componentName}__${name}__${hash}`
+      },
+    },
+  },
 })
