97c06556d4b0dde1e03f6c23943d8a1c4830d2d9
Alex <a.v.gorbach@gmail.com>
2023-12-12 17:50:07 +0400

    Write to the cache partial data

--- apps/react-frontend/src/main.tsx
+++ apps/react-frontend/src/main.tsx
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom/client'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import App from './App.tsx'
 import './index.css'
+import GET_ITEMS from './graphql/GetItems.graphql.ts'
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
   cache: new InMemoryCache(),
+})
+
+client.writeQuery({
+  query: GET_ITEMS,
+  variables: { skip: 10, take: 5 },
+  data: {
+    items: [
+      {
+        id: '10',
+        title: 'Cached data for item 10',
+      },
+    ],
+  },
 })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
