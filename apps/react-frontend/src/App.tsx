15a71dbc4aa6895f790237e48fd18973cf0af275
Alex <a.v.gorbach@gmail.com>
2023-12-12 16:01:05 +0400

    Display both components

--- apps/react-frontend/src/App.tsx
+++ apps/react-frontend/src/App.tsx
@@ -1,56 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import { useQuery, gql } from '@apollo/client'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ClassicLoad from './components/ClassicLoad/ClassicLoad.component.tsx'
+import SuspenseLoad from './components/SuspenseLoad/SuspenseLoad.component.tsx'
 
-const GET_ITEMS = gql`
-  query GetItems {
-    items {
-      id
-      title
-      description
-      completed
-    }
-  }
-`
+import styles from './App.module.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const { loading, error, data } = useQuery(GET_ITEMS)
-
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error : {error.message}</p>
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={styles.container}>
+      <div className={styles.loadContainer}>
+        <ClassicLoad />
+        <SuspenseLoad />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      {data.items.map((item: any) => (
-        <div key={item.id}>
-          <p>{item.id}</p>
-          <p>{item.title}</p>
-        </div>
-      ))}
-    </>
+    </div>
   )
 }
 
